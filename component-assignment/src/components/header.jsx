b119a56a8eec72594e28d1ffb33645d0f455cbec
nellyhuima <nelly.huima@icloud.com>
2024-11-06 16:00:09 +0200

    header created and added to the index.

--- component-assignment/src/components/header.jsx
+++ component-assignment/src/components/header.jsx
@@ -1,3 +1,17 @@
-/* add header here */
-/* styling */
-/* this is a test add to see if the branch works */
+/* add header here, check! */
+/* styling, check! */
+/* this is a test add to see if the branch works, works, check! */
+
+
+
+function Header() {
+
+    return(
+        <div class="Header">
+            <img src="/grass-7487114_1280.jpg" alt="sunset" class="round-image"></img>
+            <h1>Welcome to product page!</h1>
+        </div>
+    )
+}
+
+export default Header
